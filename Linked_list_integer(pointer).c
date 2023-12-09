ec137768749d4ff3eb645ecffcf465d989401020
Ji-ji <135002121+luminous037@users.noreply.github.com>
2023-12-09 19:24:32 +0900

    Update Linked_list_integer(pointer).c

--- Linked_list_integer(pointer).c
+++ Linked_list_integer(pointer).c
@@ -1,72 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include <stdlib.h> // 동적할당
+
 
 struct Node {
-    int num;
-    struct Node* next;
+	int num;
+	struct Node* next;
 };
 
-struct Node* first;
-struct Node* cur;
 struct Node* pre;
-struct Node* new_node;
+struct Node* first;
+struct Node* new;
+struct Node* cur;
 
 int main() {
-    first = NULL;
+	
+	int n;
+	printf("Enter integers>> ");
+	first = NULL;
 
-    int n;
+	while (1) {
 
-    printf("Enter integers>> ");
-    while (1){
-        // 새 노드를 위한 메모리 할당
-        new_node = (struct Node*)malloc(sizeof(struct Node));
+		new = malloc(sizeof(struct Node));
 
-        
-        scanf("%d", &n);
+		scanf("%d", &n);
 
-        // 음수가 입력되면 루프를 종료
-        if (n < 0) {
-            break;
-        }
+		if (n < 0)break;
 
-        // 입력된 정수를 새 노드의 num에 할당
-        new_node->num = n;
+		new->num = n;
 
-        if (first == NULL) {
-            // 리스트가 비어있으면 첫 번째 노드로 설정
-            first = new_node;
-            new_node->next = NULL;
-        }
-        else {
-            // 리스트가 비어있지 않으면 정렬된 순서에 따라 노드를 삽입
-            cur = first;
-            pre = NULL;
+		if (first == NULL) {
+			first = new;
+			new->next = NULL;
+		}
 
-            while (cur != NULL && n > cur->num) {
-                pre = cur;
-                cur = cur->next;
-            }
+		else {
+			cur = first;
+			pre = NULL;
 
-            if (pre == NULL) {
-                // 맨 앞에 삽입
-                new_node->next = first;
-                first = new_node;
-            }
-            else {
-                // 중간이나 끝에 삽입
-                pre->next = new_node;
-                new_node->next = cur;
-            }
-        }
+			while (cur != NULL && n > cur->num) {
+				pre = cur;
+				cur = cur->next;
+			}
 
-        cur = first;
-        while (cur != NULL) {
-            printf("%d (%p) ", cur->num, (void*)cur);
-            cur = cur->next;
-        }
-        printf("\n");
+			if (pre == NULL) {
+				new->next = first;
+				first = new;
+			}
 
-    }
+			else {
+				pre->next = new;
+				new->next = cur;
+			}
+		}
 
-    return 0;
+		cur = first;
+		while (cur != NULL) {
+			printf("%d (%p)", cur->num, (void*)cur);
+			cur = cur->next;
+		}
+		printf("\n");
+	}
+	return 0;
 }
