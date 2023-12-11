7551935543bc1a7d4a6bb2773ec33ad7c33bdcec
Ji-ji <135002121+luminous037@users.noreply.github.com>
2023-12-11 13:01:06 +0900

    맨 처음에 음수를 입력하면 종료되도록

--- Linked_list_integer(pointer).c
+++ Linked_list_integer(pointer).c
@@ -13,52 +13,60 @@ struct Node* new;
 struct Node* cur;
 
 int main() {
-	
+
 	int n;
-	printf("Enter integers>> ");
-	first = NULL;
+	
+	
 
 	while (1) {
+		first = NULL;
+		printf("Enter integers>> ");
+		
+		int count = 0;
 
-		new = malloc(sizeof(struct Node));
+		while (1) {
+			new = malloc(sizeof(struct Node));
+			scanf("%d", &n);
+			count++;
 
-		scanf("%d", &n);
+			if (count == 1 && n < 0)return 0;
+			if (n < 0)break;
+			
 
-		if (n < 0)break;
+			new->num = n;
 
-		new->num = n;
-
-		if (first == NULL) {
-			first = new;
-			new->next = NULL;
-		}
-
-		else {
-			cur = first;
-			pre = NULL;
-
-			while (cur != NULL && n > cur->num) {
-				pre = cur;
-				cur = cur->next;
-			}
-
-			if (pre == NULL) {
-				new->next = first;
+			if (first == NULL) {
 				first = new;
+				new->next = NULL;
 			}
 
 			else {
-				pre->next = new;
-				new->next = cur;
-			}
-		}
+				cur = first;
+				pre = NULL;
 
-		cur = first;
-		while (cur != NULL) {
-			printf("%d (%p)", cur->num, (void*)cur);
-			cur = cur->next;
+				while (cur != NULL && n > cur->num) {
+					pre = cur;
+					cur = cur->next;
+				}
+
+				if (pre == NULL) {
+					new->next = first;
+					first = new;
+				}
+
+				else {
+					pre->next = new;
+					new->next = cur;
+				}
+			}
+
+			cur = first;
+			while (cur != NULL) {
+				printf("%d (%p)", cur->num, (void*)cur);
+				cur = cur->next;
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
 	return 0;
 }
